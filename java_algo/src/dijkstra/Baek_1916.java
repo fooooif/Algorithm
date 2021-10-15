1e84e7362d0f0be4beb1cf08fe21f0dc34afb8fe
fooooif <qpqh1@naver.com>
2021-10-15 19:37:25 +0900

    add: 백준_1916

--- java_algo/src/dijkstra/Baek_1916.java
+++ java_algo/src/dijkstra/Baek_1916.java
@@ -14,52 +14,50 @@ public class Baek_1916 {
     static int start = 0;
     static int end = 0;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        visited = new int[N];
-        arr = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            N = Integer.parseInt(br.readLine());
+            visited = new int[N];
+            arr = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
 
-                arr[i][j] = Integer.MAX_VALUE;
+                    arr[i][j] = Integer.MAX_VALUE;
+                }
             }
-        }
-        M = Integer.parseInt(br.readLine());
+            M = Integer.parseInt(br.readLine());
 
 
 
-        int x = 0;
-        int y = 0;
-        int cost = 0;
-        for (int i = 0; i < M; i++) {
+            int x = 0;
+            int y = 0;
+            int cost = 0;
+            String a ;
+
+            for (int i = 0; i < M; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                x = Integer.parseInt(st.nextToken());
+                y = Integer.parseInt(st.nextToken());
+                cost = Integer.parseInt(st.nextToken());
+                if (arr[x - 1][y - 1] > cost) {
+                    arr[x-1][y-1] = cost;
+                }
+
+            }
+
             StringTokenizer st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            cost = Integer.parseInt(st.nextToken());
-            if (arr[x - 1][y - 1] > cost) {
-                arr[x-1][y-1] = cost;
-            }
+            start = Integer.parseInt(st.nextToken())-1 ;
+            end = Integer.parseInt(st.nextToken())-1 ;
+            for (int i = 0; i < N; i++) {
+                if (i != start) {
+                    visited[i] = Integer.MAX_VALUE;
+                }
+                else{
+                    visited[i] = 0;
+                }
 
-        }
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        start = Integer.parseInt(st.nextToken())-1 ;
-<<<<<<< Updated upstream
-        end = Integer.parseInt(st.nextToken())-1 ;
-        for (int i = 0; i < N; i++) {
-            if (i != start) {
-                visited[i] = Integer.MAX_VALUE;
             }
-            else{
-                visited[i] = 0;
-            }
-
-        }
-=======
-        end = Integer.parseInt(qwerqwerst.nextToken())-1 ;
->>>>>>> Stashed changes
-        dijkstra(start, end);
-        System.out.println(visited[end]);
+            dijkstra(start, end);
+            System.out.println(visited[end]);
 
     }
 
