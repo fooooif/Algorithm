ec1ec88b827f7e8e087afc5c594c8f2cca2e81e2
fooooif <qpqh1@naver.com>
2021-10-22 21:21:12 +0900

    add: 문자열 길이로 정렬하기
    StringLengthSort Comparator사용

---
+++ java_algo/src/StringLengthSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class StringLengthSort {
+    public static void main(String[] args) {
+        String[] a = new String[6];
+        a[0] = "1111111111";
+        a[1] = "1111111";
+        a[2] = "1111111";
+        a[3] = "1111";
+        a[4] = "11111111";
+        a[5] = "1111111111111";
+        Comparator<String> s = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length()>o2.length()?1:-1;
+            }
+        };
+
+        Arrays.sort(a, s);
+
+
+        for (String s1 : a) {
+            System.out.println(s1);
+        }
+    }
+}
---
+++ java_algo/src/string/Baek_5052.java
@@ -0,0 +1,56 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Baek_5052 {
+    static int t;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i < t; i++){
+
+            int n = Integer.parseInt(br.readLine());
+
+            String[] arr = new String[n];
+            for(int j = 0; j< n; j++){
+                arr[j] = br.readLine();
+            }
+            Comparator<String> c = new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return Integer.compare(o1.length(), o2.length());
+                }
+            } ;
+
+            Arrays.sort(arr, c);
+            boolean answer = false;
+            for(int a = 0 ; a<n; a++){
+                for(int b = a+1 ; b<n; b++){
+                    if (arr[a].equals(arr[b].substring(0, arr[a].length()))) {
+                        answer = true;
+                        break;
+                    }
+                }
+                if(answer == true){
+                    break;
+                }
+            }
+            if(answer == true){
+                System.out.println("NO");
+            }
+            else{
+                System.out.println("YES");
+            }
+
+
+
+        }
+    }
+
+}
