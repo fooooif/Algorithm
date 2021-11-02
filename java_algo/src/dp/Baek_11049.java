252de6eae466d63518785eca627d4c8b4e112302
fooooif <qpqh1@naver.com>
2021-11-02 19:31:04 +0900

    add: 백준 11049

--- java_algo/src/dp/Baek_11049.java
+++ java_algo/src/dp/Baek_11049.java
@@ -1,4 +1,18 @@
 package dp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Baek_11049 {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        
+    }
 }
