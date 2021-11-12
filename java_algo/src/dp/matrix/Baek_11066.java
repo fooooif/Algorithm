3deacc8e507b81ff52d63a071667f854cb3af691
fooooif <qpqh1@naver.com>
2021-11-12 15:42:01 +0900

    add: 백준 11066

---
+++ java_algo/src/dp/matrix/Baek_11066.java
@@ -0,0 +1,33 @@
+//package dp.matrix;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class Baek_11066 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int T = Integer.parseInt(br.readLine());
+//
+//        for(int i = 0 ; i < T ;i++){
+//            int k = Integer.parseInt(br.readLine());
+//            int[] arr = new int[k];
+//            int[][] dp = new int[k][k];
+//            StringTokenizer st = new StringTokenizer(br.readLine())
+//            for(int j = 0 ; j < k ; j++){
+//                arr[j] = Integer.parseInt(st.nextToken());
+//            }
+//
+//            for(int a = 0 ; a < k ;a++){
+//                dp[a][a + 1] = arr[a] + arr[a + 1];
+//            }
+//            for(int a = 0 ; a < k ;a++){
+//                for (int b = 0 ; b < k; b++){
+//
+//                }
+//            }
+//
+//        }
+//    }
+//}
