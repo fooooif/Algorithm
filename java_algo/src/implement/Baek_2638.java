95bc9e7bb777defa670660c938ec342729e7b6a9
fooooif <qpqh1@naver.com>
2021-11-15 18:55:11 +0900

    fix: 백준 2638

--- java_algo/src/implement/Baek_2638.java
+++ java_algo/src/implement/Baek_2638.java
@@ -9,6 +9,9 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Baek_2638 {
+    static int[][] arr ;
+    static int[][] dir;
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,7 +20,7 @@ public class Baek_2638 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N][M];
+        arr = new int[N][M];
 
         Queue<Node> queue = new LinkedList<>();
         for (int i = 0; i < N; i++) {
@@ -31,10 +34,13 @@ public class Baek_2638 {
                 }
             }
         }
-        int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int answer_count = 0;
         ArrayList<Node> arrayList = new ArrayList<>();
         while (!queue.isEmpty()) {
+            visited = new boolean[N][M];
+            findExter(0, 0);
+
             int size = queue.size();
             for (int i = 0 ;i < size; i++){
                 Node poll = queue.poll();
@@ -43,7 +49,7 @@ public class Baek_2638 {
                     int x = poll.x + dir[j][0];
                     int y = poll.y + dir[j][1];
                     if (x > -1 && x < arr.length && y > -1 && y < arr[0].length) {
-                        if (arr[x][y] == 0) {
+                        if (arr[x][y] == 0 && visited[x][y] == true) {
                             count +=1;
                         }
 
@@ -67,10 +73,32 @@ public class Baek_2638 {
 
 
     }
+    static void findExter(int x,int y){
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(new Node(x, y));
+        visited[x][y] = true;
+        while (!queue.isEmpty()) {
+            Node poll = queue.poll();
+
+            for (int j = 0; j < dir.length; j++) {
+                int x_x = poll.x + dir[j][0];
+                int y_y = poll.y + dir[j][1];
+                if (x_x > -1 && x_x < arr.length && y_y > -1 && y_y < arr[0].length) {
+
+                    if (visited[x_x][y_y] == false && arr[x_x][y_y] == 0) {
+                        visited[x_x][y_y] = true;
+                        queue.add(new Node(x_x, y_y));
+                    }
+
+                }
+            }
+        }
+    }
     static class Node{
         int x;
         int y;
-
+        int count = 0;
         Node(int x, int y) {
             this.x= x;
             this.y = y;
